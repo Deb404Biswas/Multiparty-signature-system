5359c95cc78ab69e09675ea555dec527ebd56a92
Debdwaipayan <debdwaipayan02@gmail.com>
2025-11-28 12:32:50 +0530

    Completed the project with logging

--- src/api/routers/parties.py
+++ src/api/routers/parties.py
@@ -6,7 +6,13 @@ from pathlib import Path
 import shutil
 from fastapi.responses import FileResponse
 import os
+from loguru import logger
 
+logger.remove()
+logger.add(
+    "app.log",
+    format="{time:MMMM D, YYYY - HH:mm:ss} {level} ----- {message}"
+)
 router=APIRouter(
     prefix='/parties/v1',
     tags=['Parties']
@@ -19,6 +25,7 @@ async def upload_image_signature(sign_image:UploadFile,party_role:str=Form(...),
     party_id:str=Form(...),session_id:str=Form(...)):
     if not await DatabaseConnect.party_collection_find_one(party_id,session_id):
         raise HTTPException(status_code=404,detail=f'{party_role} does not exists in session{session_id}')
+    logger.info(f"{party_role} with {party_id} from session {session_id} uploading image through post parties/v1/upload-image endpoint")
     msg=''
     session_folder = UPLOAD_DIR / session_id
     session_folder.mkdir(parents=True, exist_ok=True)
@@ -27,6 +34,7 @@ async def upload_image_signature(sign_image:UploadFile,party_role:str=Form(...),
     with image_file_path.open('wb') as buffer:
         shutil.copyfileobj(sign_image.file, buffer)
         await Sign_Detect_Extract.signature_detect_extract(image_file_path)
+        logger.info(f"Signature image is extracted from {sign_image.filename} and stored in {image_file_path}")
         session_doc=await DatabaseConnect.session_collection_find_one(session_id)
         parties_sign_filepath=session_doc.get('parties_sign_filepath',{})
         parties_sign_filepath[f'{party_role}_{party_id}'] = str(image_file_path)
@@ -35,8 +43,11 @@ async def upload_image_signature(sign_image:UploadFile,party_role:str=Form(...),
                 "parties_sign_filepath":parties_sign_filepath
             }}
         await DatabaseConnect.session_collection_update_one(update_data,session_id)
+        logger.info(f"{party_role} with {party_id} able to upload image.")
+        logger.info(f"The image filepath in database is : {image_file_path}")
         msg=f'Signature extracted from {sign_image.filename} provided by {party_role} with id:{party_id} in session:{session_id}'
     sign_image.file.close()
+    logger.info(f"{party_role} with id:{party_id} in session {session_id} completed the upload image process.")
     return msg
 
 @router.put('/update-image',status_code=status.HTTP_202_ACCEPTED)
@@ -44,6 +55,7 @@ async def update_image_signature(sign_image:UploadFile,party_role:str=Form(...),
     party_id:str=Form(...),session_id:str=Form(...)):
     if not await DatabaseConnect.party_collection_find_one(party_id,session_id):
         raise HTTPException(status_code=404,detail=f'{party_role} does not exists in session{session_id}')
+    logger.info(f"{party_role} with {party_id} from session {session_id} updating image through put parties/v1/update-image endpoint")
     msg=''
     session_doc=await DatabaseConnect.session_collection_find_one(session_id)
     parties_submit_status=session_doc.get('parties_submit_status',{})
@@ -54,6 +66,7 @@ async def update_image_signature(sign_image:UploadFile,party_role:str=Form(...),
     with image_file_path.open('wb') as buffer:
         shutil.copyfileobj(sign_image.file, buffer)
         await Sign_Detect_Extract.signature_detect_extract(image_file_path)
+        logger.info(f"New image : {sign_image.filename} Filepath:{image_file_path}")
         parties_sign_filepath=session_doc.get('parties_sign_filepath',{})
         parties_sign_filepath[f'{party_role}_{party_id}'] = str(image_file_path)
         update_data = {
@@ -61,8 +74,10 @@ async def update_image_signature(sign_image:UploadFile,party_role:str=Form(...),
                 "parties_sign_filepath":parties_sign_filepath
             }}
         await DatabaseConnect.session_collection_update_one(update_data,session_id)
+        logger.info(f"The update image filepath is stored in database")
         msg=f'Signature extracted from {sign_image.filename} used to update existing signature by {party_role} with id:{party_id} in session{session_id}'
     sign_image.file.close()
+    logger.info(f"{party_role} with id:{party_id} in session {session_id} completed the update image process.")
     return msg
 
 @router.put('/submit-confirmation',status_code=status.HTTP_200_OK)
@@ -70,6 +85,7 @@ async def parties_update_confirmation(party_role:str=Form(...),\
     party_id:str=Form(...),session_id:str=Form(...)):
     if not await DatabaseConnect.party_collection_find_one(party_id,session_id):
         raise HTTPException(status_code=404,detail=f'{party_role} does not exists in session{session_id}/{party_id} is incorrect for {party_role}')
+    logger.info(f"{party_role} with {party_id} from session {session_id} trying to submit conformation through put parties/v1/submit-conformation endpoint")
     session_doc=await DatabaseConnect.session_collection_find_one(session_id)
     parties_submit_status=session_doc.get('parties_submit_status',{})
     parties_sign_filepath=session_doc.get('parties_sign_filepath',{})
@@ -83,12 +99,14 @@ async def parties_update_confirmation(party_role:str=Form(...),\
                 "parties_submit_status":parties_submit_status
             }}
     await DatabaseConnect.session_collection_update_one(update_data,session_id)
+    logger.info(f"{party_role} with {party_id} from session {session_id} completed submit conformation through put parties/v1/submit-conformation endpoint")
     return f"{party_role} with id:{party_id} of session:{session_id} has submitted signature."
 
 @router.get("/download-pdf/{session_id}")
 async def download_pdf(session_id):
     if not await DatabaseConnect.session_collection_find_one(session_id):
         raise HTTPException(status_code=404,detail=f'Session is:{session_id} does not exists in record')
+    logger.info(f"session_id_{session_id}.pdf is being downloaded")
     filename=f"session_id_{session_id}.pdf"
     pdf_directory=Path(r'src\api\services\pdf_generation')
     file_path = os.path.join(pdf_directory,filename)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from src.api.routers import admin,parties
+from loguru import logger
 
+logger.remove()
+logger.add(
+    "app.log",
+    format="{time:MMMM D, YYYY - HH:mm:ss} {level} ----- {message}"
+)
+logger.info("Starting the FastApi server...")
 app=FastAPI()
-
+logger.info("Connecting to routers...")
 app.include_router(admin.router)
 app.include_router(parties.router)
